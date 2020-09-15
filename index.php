c1312a491de9316b487ff6fa4facb5b4860d6487
majaju <67795133+majaju@users.noreply.github.com>
2020-09-15 20:28:34 +0530

    index.php

--- index.php
+++ index.php
@@ -1,289 +1,116 @@
-<!DOCTYPE html>
 
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRN Registeration</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
 
-<meta charset="UTF-8">
+        .cont 
+        {   border: 1px solid black;
+            background-color: gray;
+            padding: 50px;
+            margin: 250px;
+            
+        }
+select{
+width:30%;
+padding:3px;
+border-radius:5px;
+border:1px solid white;
+outline:0;
+font-family:serif;
+font-size:18px;
+background:none;
+}
+#btn{
+background-color: #A52A2A;
+padding:3px;
+border-radius:5px;
+border:1px solid white;
+outline:0;
+font-family:serif;
+font-size:18px;
+background:none;  
+}
 
-<title>Event Registration</title>
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.4.1/css/all.css">
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-<link rel="stylesheet" href="event.css">
-<script>
-
-            function valid()
-            {
-                var fname = document.getElementById('fname').value;
-                var lname = document.getElementById('lname').value;
-                var email = document.getElementById('email').value;
-                var mobileno = document.getElementById('mobileno').value;
-                var morning = document.getElementById('morning').value;
-                var afternoon = document.getElementById('afternoon').value;
-				var evening = document.getElementById('evening').value;
-                var paintings = document.getElementById('paintings');
-
-                var atposition  = email.indexOf("@");
-                var dotposition = email.lastIndexOf(".");
-
-                if (fname === "") {
-                    return("PLEASE ENTER YOUR NAME!");
-                } else if (fname.length<3) {
-                    alert("NAME ENTERED IS TOO SMALL!");
-                } else if (fname.length>20) {
-                    return("NAME ENTERED IS TOO BIG!");
-                } else if (isNaN(fname)==false){
-                    return("NAME CANNOT BE NUMERIC!");
-                } else if (email === "") {
-                    return("PLEASE ENTER YOUR EMAIL ID!");
-                } else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length) {
-                    return("INVALID EMAIL ID! RE-ENTER IN THIS FORMAT (xxxxx@xxxxx.com)");
-                } else if (mobileno === "") {
-                    return("PLEASE ENTER YOUR PHONE NUMBER!");
-                } else if (isNaN(mobileno)==true) {
-                    return("ENTER A VALID PHONE NUMBER (ONLY DIGITS) ");
-                } else if (mobileno.length !=10 ) {
-                    return("ENTER A VALID PHONE NUMBER (10 DIGITS ONLY)");
-                } else if (!(document.getElementById('type1').checked) && !(document.getElementById('type2').checked)) {
-                    return("SELECT A OPTION")
-                }
-                else
-                  { return true;
-                  }
-            }
-
-
-              function addRow()
-              {
-                if(valid() == true)
-                {
-
-                  var fname = document.getElementById('fname').value;
-				  var lname = document.getElementById('lname').value;
-                  var mobileno = document.getElementById('mobileno').value;
-                  var email = document.getElementById('email').value;
-                  var morning = document.getElementById('morning').value;
-                var afternoon = document.getElementById('afternoon').value;
-				var evening = document.getElementById('evening').value;
-                  var paintings = document.getElementById('paintings');
-
-
-                    // get the html table
-                    // 0 = the first table
-                  var table = document.getElementsByTagName('table')[0];
-                  var newRow = table.insertRow(table.rows.length);
-
-
-
-                    var s= document.getElementById("paintings");
-                    var paintings = paintings.options[paintings.selectedIndex].value;
-
-                    if (document.getElementById('morning').checked) {
-                        e= document.getElementById('morning').value;
-                    }
-                    else if (document.getElementById('afternoon').checked) {
-                       e = document.getElementById('afternoon').value;
-                    }
-		    else if (document.getElementById('evening').checked) {
-                       e = document.getElementById('evening').value;
-                    }
-
-                    var cel1 = newRow.insertCell(0);
-                    var cel2 = newRow.insertCell(1);
-                    var cel3 = newRow.insertCell(2);
-                    var cel4 = newRow.insertCell(3);
-                    var cel5 = newRow.insertCell(4);
-                    var cel6 = newRow.insertCell(5);
-                    // add values to the cells
-                    cel1.innerHTML = fname;
-                    cel2.innerHTML = lname;
-                   cel3.innerHTML = mobileno;
-                    cel4.innerHTML = email;
-                    cel5.innerHTML = e;
-                    cel6.innerHTML = paintings;
-
-                    if(document.getElementById('type1').checked && !(document.getElementById('type2').checked))
-                    {
-                    	c1 = document.getElementById('type1').value;
-                    	var cel7 = newRow.insertCell(6);
-                    	cel7.innerHTML = c1;
-                    }
-                    else if(document.getElementById('type2').checked && !(document.getElementById('type1').checked))
-                    {
-                    	c1 = document.getElementById('type2').value;
-                    	var cel7 = newRow.insertCell(6);
-                    	cel7.innerHTML = c1;
-                    }
-                    
-                    else if(document.getElementById('type1').checked && document.getElementById('type2').checked )
-                    {
-                    	c1 = document.getElementById('type1').value;
-                    	var cel7 = newRow.insertCell(6);
-                    	cel7.innerHTML = c1;
-
-                    	c2 = document.getElementById('type2').value;
-                    	var cel8 = newRow.insertCell(7);
-                    	cel8.innerHTML = c2;
-                    }
-
-                    // add new empty row to the table
-                    // 0 = in the top
-                    // table.rows.length = the end
-                    // table.rows.length/2+1 = the center
-                    
-                    // add cells to the row
-                    
-                    
-                    
-
-                    alert("Form will be submitted");
-              }
-              else {alert(valid());}
-            }
-
-        </script>
-
-
+    </style>
 </head>
 
 <body>
+    <div class="cont">
 
-<header>
+        <h1 style="margin-left: 290px;">PRN Generation</h1>
+        <select name="select" id="select" style="margin-left: 300px;">
+            <option value="0">Select</option>
+            <option value="1">Computer Department</option>
+            <option value="2">Electronic Department</option>
+            <option value="3">Mechanical Department</option>
+            <option value="4">Civil Department</option>
+            <option value="5">Chemical Department</option>
+        </select>
+        <span><button id="btn" onclick="myfunct()">Submit</button></span>
+        <p style="margin-left:320px" >The Generated PRN:</p>
+        <span id="disp" style="margin-left:300px; color: red; font-size: 30px;"></span>
+        <p>NOTE: Ex. CS=Computer Department; <br> 01-04=Batch;  <br>1-1000=Sequence Number</p>
+    </div>
+    <script>
+        function myfunct() {
+            var select = document.getElementById("select");
+            //Department
 
-<nav class="black">
+            if (select.value == "0") {
+                alert("PLease select one of the option");
+                return false;
+            }
+            if (select.value == "1") {
+                var dept = "CS";
+            }
+            if (select.value == "2") {
+                var dept = "EX";
+            }
+            if (select.value == "3") {
+                var dept = "ME";
+            }
+            if (select.value == "4") {
+                var dept = "CV";
+            }
+            if (select.value == "5") {
+                var dept = "CH";
+            }
 
-<div class="logo">
-<img src="logo.jpeg">
-</div>
+            //Sequence NUmber
+            var a1 = 1;
+            var b1 = 999;
+            var b2 = a1 + (b1 - a1) * Math.random();
+            var no = Math.round(b2);
 
-<ul>
-<li><a href="#section1">Home</a></li>
-<li><a href="#section2">Registration Form</a></li>
-<li><a href="#section3">Show registration</a></li>
-</ul>
-</nav>
+            if (no < 10) {
+                var n = "000" + no;
+            }
+            if (no < 100 & no >= 10) {
+                var n = "00" + no;
+            }
+            if (no < 1000 & no >= 100) {
+                var n = "0" + no;
+            }
 
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+            var c1 = 1;
+            var d1 = 4;
+            var d2 = c1 + (d1 - c1) * Math.random();
+            var batch = Math.round(d2);
 
-<script type="text/javascript">
+            document.getElementById('disp').innerHTML = "20" + dept + "0" + batch + n;
 
-$(function(){
-    
-$('li').click(function(){
-        
-$('li.active').removeClass('active');
-        
-$(this).addClass('active');
-    });
-});
-</script>
-
-</header>
-
-<div id="section1" >
-<div class="container">
-<h1>Welcome to art in motion!!</h1><br>
-<p1><b>Art is not what you see, but what you make others see. Art exhibition is a great way to increase the credibility of artists. Sharing artwork is beneficial to both artist as well as visitor. Art Exhibitions can create a spark for an artists creativity. This event is all about exhibiting art. Basically the visitors have to pay for entering the exhibition and they may buy the paintings or any art if they are willing to by consulting with the owner of the painting.<b></p1>
-</div>
-</div>
-<br><br>
-<div id="section2">
-<div  class="event-registration" id="sec2">
-   <br>
-
-    <form id="event-registration-form">
-      <!-- Form contents will go here -->
-
-        <label for="fname">First Name</label><br><br>
-        <input type="text" id="fname" name="first-name" placeholder="seeta"  maxlength="45" pattern="^[a-zA-Z]+((\s{1}[a-zA-Z]+)+|$)" required autofocus/><br><br>
-        <label for="lname">Last Name</label><br><br>
-        <input type="text" id="lname" name="last-name" placeholder="hiremath"  maxlength="45" pattern="^[a-zA-Z]+((\s{1}[a-zA-Z]+)+|$)" required autofocus /><br><br>
-        <label for="phone">Mobile Number</label><br><br>
-        <input type="text" id="mobileno" name="mobileno" placeholder="mobile_number" pattern="^\+[1-9]{1}[0-9]{3,14}$" required autofocus/><br><br>
-        <label for="email">Email</label><br><br>
-        <input type="email" id="email" name="email" placeholder="seeta@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required autofocus/><br><br>
-		<br><br>
- 
- 
- <label for="time">Time slot:</label><br><br>
-  <input type="radio" id="morning" name="morning" value="morning">
-  <label for="morning">Morning</label><br><br>
-  <input type="radio" id="afternoon" name="afternoon" value="afternoon">
-  <label for="afternoon">Afternoon</label><br><br>
-  <input type="radio" id="evening" name="evening" value="evening">
-  <label "evening">Evening</label>
-<br><br><br><br>
-
- <label for="paintings">Type of painting you want to exhibit:</label><br><br>
-  <select name="paintings" id="paintings"><br><br>
-    <option value="oil">Oil</option>
-    <option value="acrylic">Acrylic</option>
-    <option value="watercolor">Watercolor</option>
-    <option value="gouache">Gouache</option>
- <option value="pastel">Pastel</option>
- <option value="encaustic">Encaustic</option>
-<option value="none">None</option>
-  </select>
-<br> <br>
-
-<label id="type" for="type">What would you like to do in art of motion:</label><br><br>
-    <input type="checkbox" id="type1" name="type1" value="Artist">
-    <label for="type1"> Exhibit Art</label>
-     <input type="checkbox" id="type2" name="type2" value="Visitor">
-    <label for="type2">Visit</label>
-
-<br><br>
- 
-
-          <button type="button" id="entry" class = "registerbtn" onclick="addRow();">SUBMIT</button>
-  </form>
- 
-
-<br><br>
-</div>
-</div>
-<div id="section3">
-<table id="table" border="1">
-   <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-       <th>Mobile Number</th>
-      <th>Email</th>
-<th>Time slot</th>
-      <th>Art you want to exhibit</th>
-     
-	  <th>Purpose of registration</th>
-      </tr>
-</table>
-</div>
-<br><br>
-
-
-
-
-
-
-
-<div  id="footer" class="footer-main-div">
-<center><div class="footer-social-icons">
-<a href="#" class="fa fa-facebook"></a>
-<a href="#" class="fa fa-youtube"></a>
-<a href="#" class="fa fa-google"></a>
-</div></center>
-
-
-<div class="footer-bottom">
-<p>Design by:<a href="#">Mansi codebuild</a></p>
-</div>
-</footer>
-
-
-
-
-
+        }
+    </script>
 </body>
-</html>
 
+</html>
